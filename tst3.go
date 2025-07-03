9d7c627235ff1465fa7a7c9b5fa39f453a944442
ondi <thy321@gmail.com>
2025-07-03 15:22:09 +0300

    minor fix

--- tst3.go
+++ tst3.go
@@ -125,14 +125,14 @@ func (self *State256_t) Uint64Next(in uint64) uint64 {
 		self.state[(y+6)%256]<<(8*6) |
 		self.state[(y+7)%256]<<(8*7))
 
-	in = in * (self.state[(x+8)%256]<<(8*0) |
-		self.state[(x+9)%256]<<(8*1) |
-		self.state[(x+10)%256]<<(8*2) |
-		self.state[(x+11)%256]<<(8*3) |
-		self.state[(x+12)%256]<<(8*4) |
-		self.state[(x+13)%256]<<(8*5) |
-		self.state[(x+14)%256]<<(8*6) |
-		self.state[(x+15)%256]<<(8*7))
+	in = in * (self.state[(x+0)%256]<<(8*0) |
+		self.state[(x+1)%256]<<(8*1) |
+		self.state[(x+2)%256]<<(8*2) |
+		self.state[(x+3)%256]<<(8*3) |
+		self.state[(x+4)%256]<<(8*4) |
+		self.state[(x+5)%256]<<(8*5) |
+		self.state[(x+6)%256]<<(8*6) |
+		self.state[(x+7)%256]<<(8*7))
 
 	return in
 }
