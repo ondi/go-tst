e1137989597da6e2b26eff62a0f7d92327ce6a56
ondi <thy321@gmail.com>
2025-08-01 17:04:02 +0300

    minor fix

--- tst3.go
+++ tst3.go
@@ -4,58 +4,57 @@
 
 package tst
 
-const (
-	FnvOffset64 = 14695981039346656037
-	FnvPrime64  = 1099511628211
-)
-
 type key3_t struct {
 	hash uint64
 	pos  int32
-	code int32
+	code byte
 }
 
-type mapped3_t[Value_t any] struct {
+type Mapped_t[Value_t any] struct {
 	value Value_t
 }
 
 type Tree3_t[Value_t any] struct {
-	root map[key3_t]*mapped3_t[Value_t]
+	root map[key3_t]*Mapped_t[Value_t]
 }
 
 func NewTree3[Value_t any]() *Tree3_t[Value_t] {
 	return &Tree3_t[Value_t]{
-		root: map[key3_t]*mapped3_t[Value_t]{},
+		root: map[key3_t]*Mapped_t[Value_t]{},
 	}
 }
 
-func (self *Tree3_t[Value_t]) Add(prefix string, value Value_t) (ok bool) {
+func (self *Tree3_t[Value_t]) Add(prefix string, value Value_t) (mapped *Mapped_t[Value_t], ok bool) {
 	var i int
-	key := key3_t{hash: FnvOffset64}
-	var temp *mapped3_t[Value_t]
-	for i, key.code = range prefix {
+	key := key3_t{}
+	state := State256_t{}
+	state.Reset()
+	for i, key.code = range []byte(prefix) {
 		key.pos = int32(i)
-		key.hash ^= uint64(key.code)
-		key.hash *= FnvPrime64
-		if temp, ok = self.root[key]; !ok {
+		state.StateNext(key.code)
+		key.hash = state.Sum64()
+		if mapped, ok = self.root[key]; !ok {
 			self.root[key] = nil
 		}
 	}
-	if temp == nil {
-		self.root[key] = &mapped3_t[Value_t]{value: value}
-		return true
+	if mapped == nil {
+		mapped = &Mapped_t[Value_t]{value: value}
+		self.root[key] = mapped
+		ok = true
 	}
-	return false
+	return
 }
 
 func (self *Tree3_t[Value_t]) Search(in string) (value Value_t, length int, found int) {
 	var ok bool
-	var temp *mapped3_t[Value_t]
-	key := key3_t{hash: FnvOffset64}
-	for length, key.code = range in {
+	var temp *Mapped_t[Value_t]
+	state := State256_t{}
+	key := key3_t{}
+	state.Reset()
+	for length, key.code = range []byte(in) {
 		key.pos = int32(length)
-		key.hash ^= uint64(key.code)
-		key.hash *= FnvPrime64
+		state.StateNext(key.code)
+		key.hash = state.Sum64()
 		if temp, ok = self.root[key]; !ok {
 			return
 		}
@@ -65,4 +64,131 @@ func (self *Tree3_t[Value_t]) Search(in string) (value Value_t, length int, foun
 		}
 	}
 	return
+}
+
+type State256_t struct {
+	state [256]uint64
+	a, b  uint64
+	c, d  uint64
+}
+
+func (self *State256_t) Reset() {
+	self.state = [256]uint64{
+		0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+		16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+		32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47,
+		48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63,
+		64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79,
+		80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95,
+		96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111,
+		112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127,
+		128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143,
+		144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159,
+		160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175,
+		176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191,
+		192, 193, 194, 195, 196, 197, 198, 199, 200, 201, 202, 203, 204, 205, 206, 207,
+		208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 223,
+		224, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234, 235, 236, 237, 238, 239,
+		240, 241, 242, 243, 244, 245, 246, 247, 248, 249, 250, 251, 252, 253, 254, 255,
+	}
+	self.a, self.b = 0, 0
+	self.c, self.d = 0, 0
+}
+
+func (self *State256_t) StateNext(in byte) {
+	// self.a = (self.a + 3) % 256
+	// self.c = (self.c + 253) % 256
+	self.a = (self.a + 5) % 256
+	self.c = (self.c + 251) % 256
+	// self.a = (self.a + 7) % 256
+	// self.c = (self.c + 249) % 256
+	self.b = (self.state[self.b] + self.state[self.a] + uint64(in) + 1) % 256
+	self.d = (self.state[self.d] + self.state[self.c] + uint64(in) + 1) % 256
+	self.state[self.a], self.state[self.b] = self.state[self.b], self.state[self.a]
+	self.state[self.c], self.state[self.d] = self.state[self.d], self.state[self.c]
+}
+
+func (self *State256_t) Sum64() (res uint64) {
+	var step uint64
+	for step < 256 {
+		step, res, _ = self.Operation(step, res)
+	}
+	return
+}
+
+func (self *State256_t) Operation(prev_step uint64, prev_val uint64) (next_step uint64, next_val uint64, a [4]uint64) {
+	a[0], a[1], a[2], a[3] = self.Uint64LE(prev_step+0, 1), self.Uint64LE(prev_step+8, 1), self.Uint64LE(prev_step+16, 1), self.Uint64LE(prev_step+24, 1)
+	next_val = (ROR64(prev_val, a[0]%64) ^ ROR64(a[1], a[2]%64)) + ROR64(a[3], a[0]%64)
+	next_val = (ROR64(next_val, a[1]%64) * ROR64(a[2], a[3]%64)) ^ ROR64(a[0], a[1]%64)
+	next_val = (ROR64(next_val, a[2]%64) + ROR64(a[3], a[0]%64)) * ROR64(a[1], a[2]%64)
+	next_val = (ROR64(next_val, a[3]%64) ^ ROR64(a[0], a[1]%64)) + ROR64(a[2], a[3]%64)
+	next_step = prev_step + 32
+	return
+}
+
+func (self *State256_t) Operation02(prev_step uint64, prev_val uint64) (next_step uint64, next_val uint64, a [4]uint64) {
+	for i := 0; i < 256; i++ {
+		next_val = ROR64(next_val, 8)
+		switch i % 3 {
+		case 0:
+			next_val = next_val ^ self.state[i]
+		case 1:
+			next_val = next_val + self.state[i]
+		case 2:
+			next_val = next_val * self.state[i]
+		}
+	}
+	next_step = 256
+	return
+}
+
+// 44
+func (self *State256_t) Operation01(prev_step uint64, prev_val uint64) (next_step uint64, next_val uint64, a [4]uint64) {
+	a[0], a[1], a[2], a[3] = self.Uint64LE(prev_step+0, 1), self.Uint64LE(prev_step+8, 1), self.Uint64LE(prev_step+16, 1), self.Uint64LE(prev_step+24, 1)
+	next_val = ROR64(prev_val, a[0]%64) ^ ROR64(a[1], a[2]%64)
+	next_val = ROR64(next_val, a[3]%64) * ROR64(a[0], a[1]%64)
+	next_val = ROR64(next_val, a[2]%64) + ROR64(a[3], a[0]%64)
+	next_val = ROR64(next_val, a[1]%64) * ROR64(a[2], a[3]%64)
+	next_step = prev_step + 32
+	return
+}
+
+// 0x12345678 <-> []{0x78, 0x56, 0x34, 0x12}
+func (self *State256_t) Uint64LE(i uint64, step uint64) uint64 {
+	return self.state[(i+0*step)%256]<<(8*0) |
+		self.state[(i+1*step)%256]<<(8*1) |
+		self.state[(i+2*step)%256]<<(8*2) |
+		self.state[(i+3*step)%256]<<(8*3) |
+		self.state[(i+4*step)%256]<<(8*4) |
+		self.state[(i+5*step)%256]<<(8*5) |
+		self.state[(i+6*step)%256]<<(8*6) |
+		self.state[(i+7*step)%256]<<(8*7)
+}
+
+// 0x12345678 <-> []{0x12, 0x34, 0x56, 0x78}
+func (self *State256_t) Uint64BE(i uint64, step uint64) uint64 {
+	return self.state[(i+0*step)%256]<<(8*7) |
+		self.state[(i+1*step)%256]<<(8*6) |
+		self.state[(i+2*step)%256]<<(8*5) |
+		self.state[(i+3*step)%256]<<(8*4) |
+		self.state[(i+4*step)%256]<<(8*3) |
+		self.state[(i+5*step)%256]<<(8*2) |
+		self.state[(i+6*step)%256]<<(8*1) |
+		self.state[(i+7*step)%256]<<(8*0)
+}
+
+func Forward(size uint64, current uint64, offset uint64) uint64 {
+	return (size + current + offset) % size
+}
+
+func Backward(size uint64, current uint64, offset uint64) uint64 {
+	return (size + current - offset) % size
+}
+
+func ROR64(in uint64, shift uint64) uint64 {
+	return (in >> shift) | (in << (64 - shift))
+}
+
+func ROL64(in uint64, shift uint64) uint64 {
+	return (in >> (64 - shift)) | (in << shift)
 }
--- tst3_test.go
+++ tst3_test.go
@@ -1,14 +1,26 @@
 //
-//
+// go test ./... -manual -v -count=1 -timeout 0
 //
 
 package tst
 
 import (
+	"flag"
+	"fmt"
+	"hash/fnv"
+	"io"
+	"math/rand/v2"
+	"os"
+	"runtime"
+	"sync"
 	"testing"
+	"time"
 
 	"gotest.tools/assert"
 )
+
+var flag_manual = flag.Bool("manual", false, "Запускать ручные тесты")
+var collisions int
 
 func Test_Tst3_01(t *testing.T) {
 	temp := NewTree3[string]()
@@ -51,4 +63,216 @@ func Test_Tst3_01(t *testing.T) {
 
 	value, _, found = temp.Search("/metrics/page")
 	assert.Assert(t, value == "/metrics")
+}
+
+var CHARSET = []byte{
+	'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '~', '@', '#', '$', '%', '^', '&', '*', '-', '_', '/',
+	'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+	'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+}
+
+// rand.New(rand.NewPCG(uint64(time.Now().UnixNano()), 1))
+func GenerateString(rnd *rand.Rand, length int, charset []byte) (out []byte) {
+	out = make([]byte, length)
+	for i := range length {
+		out[i] = charset[rnd.IntN(len(charset))]
+	}
+	return
+}
+
+func StringToUint64(in string) uint64 {
+	h := fnv.New64a()
+	io.WriteString(h, in)
+	return h.Sum64()
+}
+
+type Shard_t struct {
+	Mx sync.Mutex
+	Dm map[uint64]string
+}
+
+type Shards_t []Shard_t
+
+func NewShards(shards int) (self Shards_t) {
+	for i := 0; i < shards; i++ {
+		self = append(self, Shard_t{Dm: map[uint64]string{}})
+	}
+	return
+}
+
+func (self Shards_t) Add(key uint64, value string) (conflict bool, value2 string, size int) {
+	shard := key % uint64(len(self))
+	// self[shard].Mx.Lock()
+	// defer self[shard].Mx.Unlock()
+	if value2, conflict = self[shard].Dm[key]; !conflict {
+		self[shard].Dm[key] = value
+	} else if value == value2 {
+		conflict = false
+	}
+	size = len(self[shard].Dm)
+	return
+}
+
+func test_02(t *testing.T, storage Shards_t, count int) {
+	fd, err := os.OpenFile("collisions.txt", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0600)
+	if err != nil {
+		fd = os.Stderr
+	} else {
+		defer fd.Close()
+	}
+
+	var repeat int
+	rnd := rand.New(rand.NewPCG(uint64(time.Now().UnixNano()), StringToUint64(t.Name())))
+	for i := 1; i < count; i++ {
+		value1 := GenerateString(rnd, 10+rnd.IntN(20), CHARSET)
+		var state State256_t
+		state.Reset()
+		for _, code := range value1 {
+			state.StateNext(code)
+		}
+		hx := state.Sum64()
+		conflict, value2, size := storage.Add(hx, string(value1))
+		if conflict {
+			collisions++
+			fmt.Fprintf(fd, "%s\t%v\t%q\t%q\n", t.Name(), i, value2, value1)
+			t.Errorf("%v collision=%v i=%v, hash=%0X, value1=%q, valuw2=%q\n", t.Name(), collisions, i, hx, value1, value2)
+		}
+		if i%1_000_000 == 0 {
+			t.Logf("%v i=%v, collision=%v, repeat=%v, storage=%v, hash=%0X, buf=%q", t.Name(), i, collisions, repeat, size, hx, value1)
+		}
+	}
+}
+
+func Test_Tst3_02(t *testing.T) {
+	if flag_manual == nil || *flag_manual == false {
+		t.Skip("skipped, add -manual to run")
+	}
+	for i := 0; i < 1_000; i++ {
+		storage := NewShards(1)
+		runtime.GC()
+		t.Run(fmt.Sprintf("test-%v", i), func(t *testing.T) { test_02(t, storage, 700_000_000) })
+	}
+}
+
+func Test_Tst3_03(t *testing.T) {
+	storage := NewShards(1)
+	t.Run("test-0", func(t *testing.T) { test_02(t, storage, 1_000_000) })
+}
+
+type DebugState_t struct {
+	A     string
+	B     string
+	Debug bool
+	Skip  bool
+}
+
+var in = []DebugState_t{
+	{A: "%G5#iW@FHT-HWfJX3", B: "tspTbAY3h$WwDQKfz"},
+	{A: "Efn-Ga-%h^UT_ZyXv", B: "kG4Xc49rTUx&0Yham"},
+	{A: "a$KYERvylDF400N~bdgfRA^o4A", B: "9Wi-rT3ddT5hz"},
+	{A: "H@dq/znBhoG%ZqL", B: "#XQwn8DIxIVSq-jxtp0PLl"},
+	{A: "N59IuxVpyp8", B: "V-4bEIL%SN#E"},
+	{A: "j1U*qQ--R3", B: "DlfXFHcgOy6s4MVGw0vNcHyaCKPp"},
+	{A: "eAJjraAxl6DZsRS_7yZMfdkqj0P$F", B: "ZLxZ0Ofk%iLfkqcCZI"},
+	{A: "NYxRswXq%rg_D@4b6rMw#", B: "IKQdkpW%pKGPOeCY"},
+	{A: "l$5x9$4R&f3pi2uUJ-/mLUV", B: "8mZloglGIj*2r#FqfQ4jcCh"},
+	{A: "U$F-7CLuOaSY^v-_R", B: "oejLo*7EROOrGMm_O5"},
+	{A: "A^Q408Jq0jFKARR~3nm1T&", B: "NdvHE$**BnalbxA"},
+	{A: "gMGpgSKyFawBt@ZTH8A9-tMwy", B: "k&Mz#uTPqGih"},
+	{A: "R/Akt4%7Np$wO5EXb4Ax&RZS96ufw", B: "6ZNNax1LTx5N6ZI"},
+	{A: "1fr#X/~kACUICqlvSNMSr", B: "Wb&~4U3WKObWmirwvrAdz&nw"},
+	{A: "z9xvjN1-L-fcKYU2H2P", B: "kTfDfs~lpgz"},
+	{A: "OQzyimP^mKyKX-Z7Q", B: "/iOsJHUMJa"},
+	{A: "TRE6dSp0cZXuJe&", B: "Cv5_un0ddi"},
+	{A: "rVnC3cbOw8pS#9u/KsImGu98", B: "p7t9kxYyzZVxDyna_fsO~t%MtHgz-"},
+	{A: "7H8xD&CnskMDuUsuxIYShi$p4l&", B: "XJD_TSlSYeawf*j9xIMpNnims~j6J"},
+	{A: "tM-heCgLC%%bMF", B: "s#FxNKsgIiukz4"},
+	{A: "vO*uJl51pJYRJ%#1we", B: "@HDNO$JwNBOq%hbI"},
+	{A: "5zISkfL@kwS^RRMcxZC-PR3ow", B: "HtAVY8kHThP"},
+	{A: "lmhBbCbaak9umO6EdQD%1D", B: "ypDIoOQZB24y%s*lk3"},
+	{A: "IrUkjAsGWk2", B: "xAJAzFS@~kv6peHX4PzPB2#OYyT"},
+	{A: "BTbUg1y/m%8JQT", B: "PAXHk%5B3V1KM1*KwcM0Dhf"},
+	{A: "k8^m#kzC3s9sq^6hBwsm2%a", B: "LCHBI#2cNxiW3ki/Vb0"},
+	{A: "H&k/mVS~/icstqm/T#98N^~&u4", B: "W-AOv76AF_"},
+	{A: "7^bUs5jGkxez-5", B: "WGZX6%5@3Jj#oChan"},
+	{A: "xK&^bZvqIJ_^CX/", B: "~Zy1a-f_@fIyd3/"},
+	{A: "5-p6GdzjeTtfoRXfC6^T", B: "&oN$*e4^Mz0@@6*D"},
+	{A: "gbPxOtHiOLJKaLH@f2sm_MCmO1z", B: "XPZ_~Jl#i4kVSMo"},
+	{A: "EGvs%fjwWQUmBs4~-fbwK$EJUkDiB", B: "EfxmTR-zuge"},
+	{A: "hA/#UAqdxX-", B: "F$FBEVyMeiFREPcCRmNJnsl$#1Y"},
+	{A: "FWLtvnwGVNZ8Tg7XQ", B: "I0hyAksuUOYS@drQLVTuU1r&c"},
+	{A: "rwfu/P~tIFSjg6Hhg2Bp/TfQT", B: "qg*s/U4P8&bULTkf&Q*kv4NP"},
+	{A: "jri#vt&%d@", B: "FRfW*Dtwyy$Y^tY-d%-2f9J*h$NN"},
+	{A: "g%hWlUYBXJ2Cy36UZ3^5", B: "xJ-jn-BmLYd"},
+	{A: "&kGua#40I73", B: "XLp%I*G0*DL"},
+	{A: "gRAJcMr2b0EOqgMOrr", B: "cZ_2K02ReNPutzl1ZfDxoxdFbaUm8"},
+	{A: "xzvOBAoxl&upRc&O2gdr$07", B: "QsAJnqQJjQZax"},
+	{A: "V74*XWIpboLsp_laNBRr0bbYD%", B: "X&PCLHI3@zVGQJNUae$X"},
+	{A: "nfg*u~2&%*@$lUTWQQ/8", B: "8KQ6hHz0X//QmC0i0L*N"},
+	{A: "RMV/2QMn%9FSN#gX8p3", B: "5qnRLiSvS#3m~&IZdEj"},
+	{A: "Lr79pkaFv&pgDlFxbKce9aGXe", B: "53ScqZakIdzFu5&-IY"},
+	{A: "GWPnCq/X&b@-WKXf^", B: "IgV~CeSNJg/1_M"},
+	{A: "~9lUozGi#5~-jL-J", B: "^WCcFmxvbrPh"},
+	{A: "MhM5cxXFMYGUQIH_zqx^nbo2", B: "jbLK/KOvmZb"},
+	{A: "3f*k#~@Jf8h&s&9P*/BGA$01^M", B: "3*gRt_EV7w6z$RB@WdakoO"},
+}
+
+func Test_Tst3_04(t *testing.T) {
+	for _, v := range in {
+		var state1, state2 State256_t
+		state1.Reset()
+		state2.Reset()
+		for _, code := range []byte(v.A) {
+			state1.StateNext(code)
+		}
+		for _, code := range []byte(v.B) {
+			state2.StateNext(code)
+		}
+		res1 := state1.Sum64()
+		res2 := state2.Sum64()
+
+		if res1 == res2 || v.Debug {
+			same := map[int]int{}
+			diff := map[int]int{}
+			for i := 0; i < 256; i++ {
+				if state1.state[i] == state2.state[i] {
+					same[i/8]++
+				} else {
+					diff[i/8]++
+				}
+			}
+
+			t.Logf("res1=%v\tlen1=%v\ta1=%v\tb1=%v\tin1=%q", res1, len(v.A), state1.a, state1.b, v.A)
+			t.Logf("res2=%v\tlen2=%v\ta2=%v\tb2=%v\tin2=%q", res2, len(v.B), state2.a, state2.b, v.B)
+			t.Logf("same = %v %v", len(same), same)
+			t.Logf("diff = %v %v", len(diff), diff)
+
+			var step1, step2 uint64
+			var h1, h2 [1]uint64
+			var a1, a2 [4]uint64
+
+			for step1 < 256 {
+				step1, h1[0], a1 = state1.Operation(step1, h1[0])
+				step2, h2[0], a2 = state2.Operation(step2, h2[0])
+
+				t.Logf("hash1=%v step1=%v a1=%v", h1[0], step1, a1)
+				t.Logf("hash2=%v step2=%v a2=%v", h2[0], step2, a2)
+
+				t.Logf("##### %v %v", h1[0] == h2[0], a1[0] == a2[0] || a1[1] == a2[1] || a1[2] == a2[2] || a1[3] == a2[3])
+			}
+			assert.Assert(t, v.Skip || v.Debug)
+		}
+	}
+}
+
+func Test_Tst3_05(t *testing.T) {
+	in := "N*1Krz%2*sP&$#0nh"
+
+	var state State256_t
+	state.Reset()
+	for _, code := range []byte(in) {
+		state.StateNext(code)
+	}
+	res := state.Sum64()
+	t.Logf("RES=%0X", res)
 }
