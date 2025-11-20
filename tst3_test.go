f8a5950d45cc86bcb5a5236db15d82ec115d0803
Sergey Klimkin <klimkin.sergey@mvideo.ru>
2025-11-21 00:07:23 +0300

    minor fix

--- tst3_test.go
+++ tst3_test.go
@@ -487,6 +487,7 @@ var in = []DebugState_t{
 	{A: "aazJk#UQBJ^NSttNjs%nMy*2T", B: "*FS/Sjnzp##"},
 	{A: "Q_Jf5wQF7I@wI4%rE_G", B: "trCBw%_-sKql$~"},
 	{A: "GzJP-uy1UDt", B: "~@qYFop9TdH2j1C"},
+	{A: "eJ&q%bQualir@#v3y2ufSE", B: "3pfhfPPxcuU$CIKINI^nPlWdm"},
 }
 
 type Res_t struct {
