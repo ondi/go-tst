2b5e2d90a2d54e9ba33556c08627ff09b24cf037
klimkin.sergey <klimkin.sergey@mvideo.ru>
2025-11-27 14:53:25 +0300

    a

--- tst3.go
+++ tst3.go
@@ -97,7 +97,7 @@ func (self *State256_t) Reset() {
 func (self *State256_t) StateAdd(in byte) uint64 {
 	self.a = (self.a + 1) % 256
 	self.b = (self.a + 1 + (self.state[self.b]+self.state[in]+uint64(in))%255) % 256
-	// self.b = (self.a + 1 + (self.state[self.b]+self.state[in]+uint64(in)+1)%(256-self.a)) % 256
+	// self.b = (self.a + 1 + (self.state[self.b]+self.state[in]+uint64(in))%(256-self.a)) % 256
 	self.c = ROL64(self.c*self.state[(self.b+self.a)%256]^self.state[self.b], 64, self.state[self.b])
 	self.state[self.a], self.state[self.b] = self.state[self.b], self.state[self.a]
 	return self.c
